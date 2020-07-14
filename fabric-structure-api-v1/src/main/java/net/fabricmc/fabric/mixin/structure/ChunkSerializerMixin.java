166f7084a3abb1eb772187d2caf3ded6dad8fb2d
i509VCB <git@i509.me>
2020-07-13 22:39:37 -0500

    Implement structure API to fix bug with missing structures

---
+++ fabric-structure-api-v1/src/main/java/net/fabricmc/fabric/mixin/structure/ChunkSerializerMixin.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.mixin.structure;
+
+import java.util.Map;
+
+import com.google.common.collect.BiMap;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.world.ChunkSerializer;
+
+@Mixin(ChunkSerializer.class)
+public abstract class ChunkSerializerMixin {
+	@Shadow
+	@Final
+	private static Logger LOGGER;
+
+	/**
+	 * @author i509VCB
+	 * @reason Emit log error when we find a missing structure.
+	 */
+	@Redirect(method = "readStructureReferences", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/BiMap;get(Ljava/lang/Object;)Ljava/lang/Object;"))
+	private static <K, V> V emitMissingStructureMessage(BiMap<K, V> map, K key) {
+		if (!map.containsKey(key)) {
+			LOGGER.error("Found missing structure reference {}! Ignoring this structure.", key);
+		}
+
+		return map.get(key);
+	}
+
+	/**
+	 * @author i509VCB
+	 * @reason Do not load missing structures. Missing structures can cause lag, endless logs of exceptions and make chunks unable to save.
+	 */
+	@Redirect(method = "readStructureReferences", at = @At(value = "INVOKE", target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"))
+	private static <K, V> V ignoreMissingStructures(Map<K, V> map, K key, V value) {
+		if (key == null) {
+			// The key is null, do not add to the map
+			return null;
+		}
+
+		return map.put(key, value);
+	}
+}
+
