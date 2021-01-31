7acaa730966a2c04d9be74c89a3e251e2478c7f8
i509VCB <git@i509.me>
2021-01-30 22:52:52 -0600

    Create a simple renderer api testmod

--- fabric-renderer-api-v1/src/testmod/java/net/fabricmc/fabric/test/renderer/RendererTest.java
+++
@@ -1,9 +0,0 @@
-package net.fabricmc.fabric.test.renderer;
-
-import net.fabricmc.api.ModInitializer;
-
-public final class RendererTest implements ModInitializer {
-	@Override
-	public void onInitialize() {
-	}
-}
